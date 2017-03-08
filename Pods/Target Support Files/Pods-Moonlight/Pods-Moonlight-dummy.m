b1164b58cd7cd6dde6f8f3efe40ffbbea2cb3bd5
Khant Zaw Ko <khantzawko7@gmail.com>
2017-03-09 01:07:52 +0630

    setup pod

---
+++ Pods/Target Support Files/Pods-Moonlight/Pods-Moonlight-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Moonlight : NSObject
+@end
+@implementation PodsDummy_Pods_Moonlight
+@end
---
+++ Pods/Target Support Files/Pods-MoonlightUITests/Pods-MoonlightUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MoonlightUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MoonlightUITests
+@end
